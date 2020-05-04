aa82b933a9b05aa3f8b01edc46c5311b4fd48f55
MiohitoKiri5474 <lltzpp@gmail.com>
2020-05-04 16:51:50 +0800

    Site updated: 2020-05-04 16:51:48

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon May 04 2020 16:50:46 GMT+0800 (Taipei Standard Time)
+// build time:Mon May 04 2020 16:51:45 GMT+0800 (Taipei Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
